44b49ce501c72517ac08754c81e5e999dc933ec8
Shannya <niveyro101@gmail.com>
2023-03-10 00:41:58 -0500

    port added

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const app = express();
+const app = express;
 const path=require('path');
 
+const PORT = process.env.PORT || 3001;
 //Middleware
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+//app.use(express.json());
+//app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/public/index.html'));
 })
+
+app.listen(PORT, () => console.log('App listening on ${PORT}'))
