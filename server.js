b64450753958ddb78c9546943f95e87838aba590
Shannya <niveyro101@gmail.com>
2023-03-10 17:14:39 -0500

    get api done

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const path=require('path');
 
+//DATA
+const notes = require('./db/db.json')
+
 const PORT = process.env.PORT || 3001;
 //Middleware
 //app.use(express.json());
@@ -11,7 +14,12 @@ app.use(express.static('public'));
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/public/index.html'));
 })
+
 app.get('/notes',(req,res)=>{
     res.sendFile(path.join(__dirname,'/public/notes.html'));
+})
+
+app.get('/api/notes',(req,res)=>{
+    res.json(notes)
 })
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
