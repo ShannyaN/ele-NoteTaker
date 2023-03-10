43e5f564f6e185af5f79024771c1e16443719805
Shannya <niveyro101@gmail.com>
2023-03-10 00:55:33 -0500

    deployment issues

--- server.js
+++ server.js
@@ -12,4 +12,4 @@ app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/public/index.html'));
 })
 
-app.listen(PORT, () => console.log('App listening on ${PORT}'))
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
