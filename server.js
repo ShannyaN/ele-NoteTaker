b9f33e705aa42adbaad075c81a705c59da1de109
Shannya <niveyro101@gmail.com>
2023-03-10 18:50:18 -0500

    resolving post

--- server.js
+++ server.js
@@ -14,23 +14,29 @@ app.use(express.static('public'));
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/public/index.html'));
+    return console.log("success");
 })
 
 app.get('/notes',(req,res)=>{
     res.sendFile(path.join(__dirname,'/public/notes.html'));
+    return console.log("success");
 })
 
 app.get('/api/notes',(req,res)=>{
     res.json(notes);
+    return console.log("success")
 })
 app.get('/api/notes/:title',(req,res)=>{
     for (let i = 0; i < notes.length; i++) {
         if (req.params.title === notes[i].title){
             res.json(notes[i])
-        };
-        
+            return console.log("success")
+        }else{
+            return res.status(404)};
     }
+    
 })
+
 app.post('/api/notes', (req,res)=>{
     const {title} = req.body.title;
     const {text} = req.body.text;
@@ -38,11 +44,14 @@ app.post('/api/notes', (req,res)=>{
         title,
         text
     }
+    console.log(newNote);
     notes.push(newNote);
+    console.log(notes)
     const notesJSON = JSON.stringify(notes, null, 2);
     fs.writeFile('./db/db.json', notesJSON)
         .then(()=> {
             res.json(newNote);
         })
 })
+
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
